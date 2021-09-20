f16938f7e4dc1a6f53d16f7cf64238eebcadf017
4skinSkywalker <stopchemtrailsfred@gmail.com>
2021-09-20 19:26:48 +0200

    Implement the 7th stimulus

--- v2/main.js
+++ v2/main.js
@@ -9,11 +9,15 @@ let faceEls = [...document.querySelectorAll(".cube > .face")];
 let innerCube = document.querySelector(".inner-cube");
 let innerFaceEls = [...document.querySelectorAll(".inner-cube > .face")];
 
-let checkCornerBtn = document.querySelector(".check-corner");
+let shape = document.querySelector(".shape");
+
 let checkCameraBtn = document.querySelector(".check-camera");
 let checkFaceBtn = document.querySelector(".check-face");
-let checkSoundBtn = document.querySelector(".check-sound");
 let checkPositionBtn = document.querySelector(".check-position");
+
+let checkShapeBtn = document.querySelector(".check-shape");
+let checkCornerBtn = document.querySelector(".check-corner");
+let checkSoundBtn = document.querySelector(".check-sound");
 let checkColorBtn = document.querySelector(".check-color");
 
 let nBackInput = document.querySelector("#n-back");
@@ -29,13 +33,13 @@ let previousLevelThresholdInput = document.querySelector("#previousLevelThreshol
 let nextLevelThresholdInput = document.querySelector("#nextLevelThreshold");
 
 let [
-  cornerEnableTrig,
   cameraEnableTrig,
   faceEnableTrig,
   positionEnableTrig,
+  shapeEnableTrig,
+  cornerEnableTrig,
   soundEnableTrig,
-  colorEnableTrig,
-  rotationEnableTrig
+  colorEnableTrig
 ] = [...document.querySelectorAll(".toggle-trigger")];
 
 // Game settings
@@ -44,7 +48,6 @@ let points = [
   "-20&0", "-20&-45", "-20&-90"
 ]; // I know this is ugly AF...
 let numbers = "123456";
-let letters = "abflqy";
 let initialCubePosition = "-.5,-3,.5";
 let moves = [
   "-3.5,0,-2.5", "-.5,0,-2.5", "2.5,0,-2.5",
@@ -59,6 +62,8 @@ let moves = [
   "-3.5,-6,.5", "-.5,-6,.5", "2.5,-6,.5",
   "-3.5,-6,3.5", "-.5,-6,3.5", "2.5,-6,3.5"
 ];
+
+let shapeClasses = ["triangle", "square", "circle"];
 let initialInnerCubePosition = ".5,.5,0";
 let cornersList = [
   "0,0,.5",
@@ -71,31 +76,12 @@ let cornersList = [
   "1,1,-.5",
   "1,1,.5"
 ];
+let letters = "abflqy";
 let colorClasses = [
   "col-a", "col-b", "col-c", "col-d", "col-e", "col-f"
 ];
 
 // Editable settings
-// This piece of code is a bit confusing I know...
-let cornerEnabled = true;
-cornerEnableTrig.checked = cornerEnabled;
-cornerEnableTrig.addEventListener("input", () =>
-  (
-    cornerEnabled = !cornerEnabled,
-    !cornerEnabled ?
-      (
-        innerCube.style.display = "none",
-        checkCornerBtn.style.display = "none"
-      ) :
-      (
-        innerCube.style.display = "block",
-        checkCornerBtn.style.display = "inline-block"
-      ),
-    innerFaceEls.forEach(face => face.style.animationDelay = "0s"),
-    checkCornerBtn.style.animationDelay = "0s"
-  )
-);
-
 let cameraEnabled = true;
 cameraEnableTrig.checked = cameraEnabled;
 cameraEnableTrig.addEventListener("input", () =>
@@ -120,18 +106,6 @@ faceEnableTrig.addEventListener("input", () =>
   )
 );
 
-let soundEnabled = true;
-soundEnableTrig.checked = faceEnabled;
-soundEnableTrig.addEventListener("input", () =>
-  (
-    soundEnabled = !soundEnabled,
-    !soundEnabled ?
-      checkSoundBtn.style.display = "none" :
-      checkSoundBtn.style.display = "inline-block",
-    checkSoundBtn.style.animationDelay = "0s"
-  )
-);
-
 let positionEnabled = true;
 positionEnableTrig.checked = faceEnabled;
 positionEnableTrig.addEventListener("input", () =>
@@ -141,6 +115,65 @@ positionEnableTrig.addEventListener("input", () =>
       checkPositionBtn.style.display = "none" :
       checkPositionBtn.style.display = "inline-block",
     checkPositionBtn.style.animationDelay = "0s"
+  )
+);
+
+// Shape disappears with corner
+let shapeEnabled = true;
+let cornerEnabled = true;
+
+shapeEnableTrig.checked = shapeEnabled;
+cornerEnableTrig.checked = cornerEnabled;
+
+function shapeEnableTrigHandler() {
+  shapeEnabled = !shapeEnabled;
+
+  if (!shapeEnabled) {
+    checkShapeBtn.style.display = "none";
+  } else {
+    checkShapeBtn.style.display = "inline-block";
+  }
+
+  checkShapeBtn.style.animationDelay = "0s";
+}
+
+cornerEnableTrig.addEventListener("input", () => {
+  cornerEnabled = !cornerEnabled;
+  
+  if (!cornerEnabled) {
+    shapeEnableTrig.removeEventListener("input", shapeEnableTrigHandler);
+    
+    shapeEnabled = false;
+    if (shapeEnableTrig.checked) {
+      shapeEnableTrig.click();
+    }
+    shapeEnableTrig.disabled = true;
+    
+    innerCube.style.display = "none";
+    checkCornerBtn.style.display = "none";
+    checkShapeBtn.style.display = "none";
+  } else {
+    shapeEnableTrig.addEventListener("input", shapeEnableTrigHandler);
+    shapeEnableTrig.disabled = false;
+    
+    innerCube.style.display = "block";
+    checkCornerBtn.style.display = "inline-block";
+  }
+  
+  innerFaceEls.forEach(face => face.style.animationDelay = "0s"),
+  checkCornerBtn.style.animationDelay = "0s"
+});
+shapeEnableTrig.addEventListener("input", shapeEnableTrigHandler);
+
+let soundEnabled = true;
+soundEnableTrig.checked = faceEnabled;
+soundEnableTrig.addEventListener("input", () =>
+  (
+    soundEnabled = !soundEnabled,
+    !soundEnabled ?
+      checkSoundBtn.style.display = "none" :
+      checkSoundBtn.style.display = "inline-block",
+    checkSoundBtn.style.animationDelay = "0s"
   )
 );
 
@@ -154,12 +187,6 @@ colorEnableTrig.addEventListener("input", () =>
       checkColorBtn.style.display = "inline-block",
     checkColorBtn.style.animationDelay = "0s"
   )
-);
-
-let rotationEnabled = false;
-rotationEnableTrig.checked = rotationEnabled;
-rotationEnableTrig.addEventListener("input", () =>
-  rotationEnabled = !rotationEnabled
 );
 
 function setFloorBackground(floor, dimPercent, tileAHexColor, tileBHexColor) {
@@ -314,32 +341,41 @@ let intervals = [];
 
 let isRunning = false;
 
-let enableCornerCheck = true;
+// I know I should use objects here, I will re-factor it at some point...
 let enableCameraCheck = true;
 let enableFaceCheck = true;
-let enableSoundCheck = true;
 let enablePositionCheck = true;
+
+let enableShapeCheck = true;
+let enableCornerCheck = true;
+let enableSoundCheck = true;
 let enableColorCheck = true;
 
-let currCorner;
 let currCamera;
 let currFace;
-let currSound;
 let currPosition;
+
+let currShape;
+let currCorner;
+let currSound;
 let currColor;
 
-let rightCorner = 0;
 let rightCamera = 0;
 let rightFace = 0;
-let rightSound = 0;
 let rightPosition = 0;
+
+let rightShape = 0;
+let rightCorner = 0;
+let rightSound = 0;
 let rightColor = 0;
 
-let wrongCorner = 0;
 let wrongCamera = 0;
 let wrongFace = 0;
-let wrongSound = 0;
 let wrongPosition = 0;
+
+let wrongShape = 0;
+let wrongCorner = 0;
+let wrongSound = 0;
 let wrongColor = 0;
 
 function random(iterable) {
@@ -421,45 +457,55 @@ function createBlocks(symbols, n) {
 function resetPoints() {
   matchingStimuli = 0;
   
-  rightCorner = 0;
   rightCamera = 0;
   rightFace = 0;
-  rightSound = 0;
   rightPosition = 0;
+  
+  rightShape = 0;
+  rightCorner = 0;
+  rightSound = 0;
   rightColor = 0;
   
-  wrongCorner = 0;
   wrongCamera = 0;
   wrongFace = 0;
-  wrongSound = 0;
   wrongPosition = 0;
+  
+  wrongShape = 0;
+  wrongCorner = 0;
+  wrongSound = 0;
   wrongColor = 0;
   
   move(cube, initialCubePosition, initialCubePosition);
   move(innerCube, initialInnerCubePosition, initialInnerCubePosition);
-  scene.style.transform = "rotateX(-40deg) rotateY(-45deg)";
+  rotateCamera(-40, -45);
 }
 
 function resetBlock() {
-  enableCornerCheck = true;
   enableCameraCheck = true;
   enableFaceCheck = true;
-  enableSoundCheck = true;
   enablePositionCheck = true;
+  
+  enableShapeCheck = true;
+  enableCornerCheck = true;
+  enableSoundCheck = true;
   enableColorCheck = true;
   
-  currCorner = null;
   currCamera = null;
   currFace = null;
-  currSound = null;
   currPosition = null;
+  
+  currShape = null;
+  currCorner = null;
+  currSound = null;
   currColor = null;
   
-  checkCornerBtn.classList.remove("right", "wrong");
   checkCameraBtn.classList.remove("right", "wrong");
   checkFaceBtn.classList.remove("right", "wrong");
-  checkSoundBtn.classList.remove("right", "wrong");
   checkPositionBtn.classList.remove("right", "wrong");
+  
+  checkShapeBtn.classList.remove("right", "wrong");
+  checkCornerBtn.classList.remove("right", "wrong");
+  checkSoundBtn.classList.remove("right", "wrong");
   checkColorBtn.classList.remove("right", "wrong");
 }
 
@@ -469,9 +515,11 @@ function resetIntervals() {
   );
 }
 
-let prevCubeZRot = 0;
-let prevCubeYRot = 0;
-let prevCubeXRot = 0;
+function rotateCamera(cx, cy) {
+  scene.style.transform = `rotateX(${cx}deg) rotateY(${cy}deg)`;
+  shape.style.transform = `translate(-50%, -50%) rotateY(${-cy}deg) rotateX(${-cx}deg)`;
+}
+
 function move(el, prevPosString, currPosString) {
   
   console.log(el, prevPosString, currPosString);
@@ -479,34 +527,7 @@ function move(el, prevPosString, currPosString) {
   let [px, py, pz] = prevPosString.split(",");
   let [cx, cy, cz] = currPosString.split(",");
   
-  // Calc direction to set cube rotation
-  if (rotationEnabled) {
-    let dz = cx - px;
-    let crz = (Math.abs(dz) === 1) ? 90 : 180;
-    crz = Math.sign(dz) * crz;
-
-    let dy = py - cy;
-    let cry = (Math.abs(dy) === 1) ? 90 : 180;
-    cry = Math.sign(dy) * cry;
-
-    let dx = pz - cz;
-    let crx = (Math.abs(dx) === 1) ? 90 : 180;
-    crx = Math.sign(dx) * crx;
-
-    if (dz !== 0 && dx === 0) {
-      prevCubeZRot += crz;
-    }
-
-    if (dy !== 0 && dx === 0 && dz === 0) {
-      prevCubeYRot += cry;
-    }
-
-    if (dx !== 0 && dz === 0) {
-      prevCubeXRot += crx;
-    }
-  }
-  
-  el.style.transform = `translate3d(${cx}em, ${cy}em, ${cz}em) rotateZ(${prevCubeZRot}deg) rotateY(${prevCubeYRot}deg) rotateX(${prevCubeXRot}deg)`;
+  el.style.transform = `translate3d(${cx}em, ${cy}em, ${cz}em)`;
 }
 
 function wow(htmlElement, cssClass, delay) {
@@ -526,31 +547,31 @@ function speak(text) {
 
 function getGameCycle(n) {
   
-  let corners;
-  if (cornerEnabled) {
-    corners = createBlocks(cornersList, n);
-  }
-  
   let cameras;
   if (cameraEnabled) {
     cameras = createBlocks(points, n);
   }
-  
   let faces;
   if (faceEnabled) {
     faces = createBlocks(numbers, n);
   }
-  
-  let sounds;
-  if (soundEnabled) {
-    sounds = createBlocks(letters, n);
-  }
-  
   let positions;
   if (positionEnabled) {
     positions = createBlocks(moves, n);
   }
   
+  let shapes;
+  if (shapeEnabled) {
+    shapes = createBlocks(shapeClasses, n);
+  }
+  let corners;
+  if (cornerEnabled) {
+    corners = createBlocks(cornersList, n);
+  }
+  let sounds;
+  if (soundEnabled) {
+    sounds = createBlocks(letters, n);
+  }
   let colors;
   if (colorEnabled) {
     colors = createBlocks(colorClasses, n);
@@ -571,20 +592,24 @@ function getGameCycle(n) {
     if (i > length - 1) {
       
       let percentage = 0;
-      if (cornerEnabled) {
-        percentage += rightCorner;
-      }
       if (cameraEnabled) {
         percentage += rightCamera;
       }
       if (faceEnabled) {
         percentage += rightFace;
       }
-      if (soundEnabled) {
-        percentage += rightSound;
-      }
       if (positionEnabled) {
         percentage += rightPosition;
+      }
+      
+      if (cornerEnabled) {
+        percentage += rightCorner;
+        if (shapeEnabled) {
+          percentage += rightShape;
+        }
+      }
+      if (soundEnabled) {
+        percentage += rightSound;
       }
       if (colorEnabled) {
         percentage += rightColor;
@@ -592,20 +617,24 @@ function getGameCycle(n) {
       percentage /= matchingStimuli;
       
       let mistakes = 0;
-      if (cornerEnabled) {
-        mistakes += wrongCorner;
-      }
       if (cameraEnabled) {
         mistakes += wrongCamera;
       }
       if (faceEnabled) {
         mistakes += wrongFace;
       }
-      if (soundEnabled) {
-        mistakes += wrongSound;
-      }
       if (positionEnabled) {
         mistakes += wrongPosition;
+      }
+      
+      if (cornerEnabled) {
+        mistakes += wrongCorner;
+        if (shapeEnabled) {
+          mistakes += wrongShape;
+        }
+      }
+      if (soundEnabled) {
+        mistakes += wrongSound;
       }
       if (colorEnabled) {
         mistakes += wrongColor;
@@ -643,19 +672,10 @@ function getGameCycle(n) {
     // Count stimulus
     stimuliCount++;
     
-    if (cornerEnabled) {
-      currCorner = corners[i];
-      let prevCorner = (corners[i-1] || { symbol: initialInnerCubePosition });
-      move(
-        innerCube,
-        prevCorner.symbol,
-        currCorner.symbol
-      );
-    }
     if (cameraEnabled) {
       currCamera = cameras[i];
       let [cx, cy] = currCamera.symbol.split("&");
-      scene.style.transform = `rotateX(${cx}deg) rotateY(${cy}deg)`;
+      rotateCamera(cx, cy);
     }
     if (faceEnabled) {
       currFace = faces[i];
@@ -669,10 +689,6 @@ function getGameCycle(n) {
       currColor = colors[i];
       wow(faceEls[0], currColor.symbol, tileFlashTime);
     }
-    if (soundEnabled) {
-      currSound = sounds[i];
-      speak(currSound.symbol);
-    }
     if (positionEnabled) {
       currPosition = positions[i];
       let prevPosition = (positions[i-1] || { symbol: initialCubePosition });
@@ -681,6 +697,25 @@ function getGameCycle(n) {
         prevPosition.symbol,
         currPosition.symbol
       );
+    }
+    
+    if (cornerEnabled) {
+      currCorner = corners[i];
+      let prevCorner = (corners[i-1] || { symbol: initialInnerCubePosition });
+      move(
+        innerCube,
+        prevCorner.symbol,
+        currCorner.symbol
+      );
+      
+      if (shapeEnabled) {
+        currShape = shapes[i];
+        wow(shape, currShape.symbol, tileFlashTime);
+      }
+    }
+    if (soundEnabled) {
+      currSound = sounds[i];
+      speak(currSound.symbol);
     }
     
     // Increase block index
@@ -733,11 +768,7 @@ function checkHandler(sense) {
   let enable;
   
   // This part is garbage but hey I've used single vars xD
-  if (sense === "corner") {
-    curr = currCorner;
-    button = checkCornerBtn;
-    enable = enableCornerCheck;
-  } else if (sense === "camera") {
+  if (sense === "camera") {
     curr = currCamera;
     button = checkCameraBtn;
     enable = enableCameraCheck;
@@ -745,14 +776,22 @@ function checkHandler(sense) {
     curr = currFace;
     button = checkFaceBtn;
     enable = enableFaceCheck;
-  } else if (sense === "sound") {
-    curr = currSound;
-    button = checkSoundBtn;
-    enable = enableSoundCheck;
   } else if (sense === "position") {
     curr = currPosition;
     button = checkPositionBtn;
     enable = enablePositionCheck;
+  } else if (sense === "shape") {
+    curr = currShape;
+    button = checkShapeBtn;
+    enable = enableShapeCheck;
+  } else if (sense === "corner") {
+    curr = currCorner;
+    button = checkCornerBtn;
+    enable = enableCornerCheck;
+  } else if (sense === "sound") {
+    curr = currSound;
+    button = checkSoundBtn;
+    enable = enableSoundCheck;
   } else if (sense === "color") {
     curr = currColor;
     button = checkColorBtn;
@@ -763,16 +802,18 @@ function checkHandler(sense) {
     return;
   }
   
-  if (sense === "corner") {
-    enableCornerCheck = false;
-  } else if (sense === "camera") {
+  if (sense === "camera") {
     enableCameraCheck = false;
   } else if (sense === "face") {
     enableFaceCheck = false;
-  } else if (sense === "sound") {
-    enableSoundCheck = false;
   } else if (sense === "position") {
     enablePositionCheck = false;
+  } else if (sense === "shape") {
+    enableShapeCheck = false;
+  } else if (sense === "corner") {
+    enableCornerCheck = false;
+  } else if (sense === "sound") {
+    enableSoundCheck = false;
   } else if (sense === "color") {
     enableColorCheck = false;
   }
@@ -781,16 +822,18 @@ function checkHandler(sense) {
   
   if (curr.isMatching) {
     
-    if (sense === "corner") {
-      rightCorner++;
-    } else if (sense === "camera") {
+    if (sense === "camera") {
       rightCamera++;
     } else if (sense === "face") {
       rightFace++;
-    } else if (sense === "sound") {
-      rightSound++;
     } else if (sense === "position") {
       rightPosition++;
+    } else if (sense === "shape") {
+      rightShape++;
+    } else if (sense === "corner") {
+      rightCorner++;
+    } else if (sense === "sound") {
+      rightSound++;
     } else if (sense === "color") {
       rightColor++;
     }
@@ -798,16 +841,18 @@ function checkHandler(sense) {
     button.classList.add("right");
   } else {
     
-    if (sense === "corner") {
-      wrongCorner++;
-    } else if (sense === "camera") {
+    if (sense === "camera") {
       wrongCamera++;
     } else if (sense === "face") {
       wrongFace++;
-    } else if (sense === "sound") {
-      wrongSound++;
     } else if (sense === "position") {
       wrongPosition++;
+    } else if (sense === "shape") {
+      wrongShape++;
+    } else if (sense === "corner") {
+      wrongCorner++;
+    } else if (sense === "sound") {
+      wrongSound++;
     } else if (sense === "color") {
       wrongColor++;
     }
@@ -816,7 +861,7 @@ function checkHandler(sense) {
   }
 }
 
-["corner", "camera", "face", "sound", "position", "color"]
+["camera", "face", "position", "corner", "shape", "sound", "color"]
   .forEach(sense => {
     document.querySelector(".check-" + sense)
       .addEventListener(
@@ -833,16 +878,18 @@ function checkHandler(sense) {
 document.addEventListener(
   "keypress",
   evt => {
-    if (evt.code === "KeyJ") {
-      checkHandler("corner");
-    } else if (evt.code === "KeyS") {
+    if (evt.code === "KeyS") {
       checkHandler("camera");
     } else if (evt.code === "KeyD") {
       checkHandler("face");
-    } else if (evt.code === "KeyK") {
-      checkHandler("sound");
     } else if (evt.code === "KeyF") {
       checkHandler("position");
+    } else if (evt.code === "KeyH") {
+      checkHandler("shape");
+    } else if (evt.code === "KeyJ") {
+      checkHandler("corner");
+    } else if (evt.code === "KeyK") {
+      checkHandler("sound");
     } else if (evt.code === "KeyL") {
       checkHandler("color");
     } else if (evt.code === "KeyQ") {
